0a1abc9c3c56024f1f3599b81b238e24f825506c
Mansi <mansi@gmail.com>
2026-02-12 18:49:23 +0530

    updated contacted form

--- backend/routes/contact/contactResponseRoute.js
+++ backend/routes/contact/contactResponseRoute.js
@@ -13,7 +13,10 @@ route.post("/", async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 587,
-      secure: false, // TLS
+secure: false,
+tls: {
+  rejectUnauthorized: false
+},
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
